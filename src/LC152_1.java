8cea68a877e371c568e386bd7ee4276523041058
Surendhar V <v.surendharsuren222@gmail.com>
2023-08-10 22:34:46 +0530

    Added LC152_1

---
+++ src/LC152_1.java
@@ -0,0 +1,43 @@
+class LC152_1 {
+    public int findMin(int[] arr) {
+
+        if(arr[0] <= arr[arr.length-1]){
+
+            return arr[0];
+        }
+
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while(end - start > 1){
+
+            System.out.println("start " + start + "   " + "end  " + end);
+
+            int mid = (start + end )/2;
+
+            if(arr[start] <= arr[mid]){
+
+
+                start = mid;
+
+
+            }else{
+
+                end = mid;
+
+
+            }
+
+
+
+
+        }
+
+        return arr[end];
+
+
+
+    }
+
+}
